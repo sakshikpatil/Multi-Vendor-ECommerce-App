4990d57200493a29418dbd7186a2274e763ea7ca
sakshikpatil <sakshikpatil892@gmail.com>
2023-06-20 14:38:35 +0530

    Final Code

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+const app = require("./app");
+const path = require('path');
+const connectDatabase = require("./db/Database");
+
+// Handling uncaught Exception
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`shutting down the server for handling uncaught exception`);
+});
+
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "config/.env",
+  });
+}
+
+//static files
+app.use(express.static(path.join(__dirname,"./client/build")))
+app.get('*',function(req,res){
+  res.sendFile(path.join(__dirname,"./client/build/index.html"));
+});
+
+
+// connect db
+connectDatabase();
+
+// create server
+const server = app.listen(process.env.PORT, () => {
+  console.log(
+    `Server is running on http://localhost:${process.env.PORT}`
+  );
+});
+
+// unhandled promise rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Shutting down the server for ${err.message}`);
+  console.log(`shutting down the server for unhandle promise rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
